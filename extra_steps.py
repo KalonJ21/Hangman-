c72d451eb715c1785b763384e91e09b921e92c7c
Kalon Jones <77160675+KalonJ21@users.noreply.github.com>
2021-11-09 15:38:21 +0000

    Remove unnecessary lines of code from extra_steps.py

--- extra_steps.py
+++ extra_steps.py
@@ -27,28 +27,5 @@ hang_man = ['_',
                     '__\n |\n O\n/|\ \n/ \ '                
                     ]
 
-'--------------------------------------------------------------------------------------'
 
-
-#Things to do for code (some things already completed, some are for extension).
-'''
-Things to Do for Code:
-'''
-'Making an option for the game to repeat itself after you lose (do you want to play again? if-else)'
-
-'Add detailed comments to make code more explainable'
-
-'Make code to be able to ask user for categories so they can use dict.'
-
-'Figure out how to make GUI.'
-
-'Add more color'
-
-'adding a difficulty, the harder the difficulty the more likely you are to get a hint'
-
-'add functions to code'
-
-'find a way for the code to put a space for words with spaces and capital letters'
-
-'Find a way for the user to enter the entire word or phrase if they want to guess it (try and implement in GUI).'
 
